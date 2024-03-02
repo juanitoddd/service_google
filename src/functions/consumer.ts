c55dbc9c87f2385662c1b42f8c76d111e8888fdd
Juan David <juand.diazd@gmail.com>
2024-03-02 16:37:24 +0100

    UPDATE

--- src/functions/consumer.ts
+++ src/functions/consumer.ts
@@ -9,20 +9,30 @@ const consumer = async (_msg: RabbitMsg) => {
   console.log(_msg);
   const { action } = _msg
   const client = await authorize()
+  let msg: any = {
+    format: 'string',
+    payload: {}
+  }
   switch (action) {
     case 'calendar':
       const calendars = await listCalendars(client)
       console.log(calendars);
+      msg.format = 'json'
+      msg.payload = calendars
       await rabbitPublish('telegram', calendars)
       break;
     case 'events':
       const events = await listTodayEvents(client)
       console.log(events);
+      msg.format = 'json'
+      msg.payload = events
       await rabbitPublish('telegram', events)
       break;
     case 'sheets':
       const majors = await listMajors(client)
       console.log(majors);
+      msg.format = 'json'
+      msg.payload = majors
       // await rabbitPublish('telegram', majors)
       break;
     default:
